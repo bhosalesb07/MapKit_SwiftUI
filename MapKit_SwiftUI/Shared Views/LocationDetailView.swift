ab1aff9d5cbeddaa8c30f9b93c11b4db2c569d99
bhosalesb07 <bhosalesb07@gmail.com>
2026-01-05 23:44:48 +0530

    Enable marker selection on map Add edit support for destination markers Allow adding/removing markers from destination placemarks Integrate MapKit Look Around view

---
+++ MapKit_SwiftUI/Shared Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MapKit_SwiftUI
+//
+//  Created by Mac on 05/01/26.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
